9450c3292e2a000aeda414d9eca545425580a3e3
Shayan Dhanani <shayandhanani@gmail.com>
2014-12-01 16:03:07 -1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,6 +6,6 @@ import re
 from lxml import etree
 from pyquery import PyQuery as pq
 
-scrape_url = "http://www.tripadvisor.com/Attractions-g60616-Activities-Kapaa_Kauai_Hawaii.html"
+scrape_url = "http://www.tripadvisor.com/Attraction_Review-g60616-d1015347-Reviews-Kayak_Wailua-Kapaa_Kauai_Hawaii.html"
 
 print scraperwiki.scrape(scrape_url)
