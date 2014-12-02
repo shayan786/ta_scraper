e55aba9dd66f7d0b53746cca41122ac76e4a0940
Shayan Dhanani <shayandhanani@gmail.com>
2014-12-01 16:10:17 -1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,11 +8,19 @@ from pyquery import PyQuery as pq
 
 scrape_url = "http://www.tripadvisor.com/Attraction_Review-g60616-d1015347-Reviews-Kayak_Wailua-Kapaa_Kauai_Hawaii.html"
 
+def get_url(url):
+    req = urllib2.Request(url, None, header)
+    response = urllib2.urlopen(req)
+    root = pq(response.read().decode('utf-8'))
+    return root
+
 def strip_tags(value):
     """Returns the given HTML with all tags stripped."""
     if value:
         return re.sub(r'<[^>]*?>', '', value)
     return ""
+    
+page = get_url(page_url)
     
 email_raw = strip_tags(page(".sprite-grayEmail").next().attr("onclick"))
 
