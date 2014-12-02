67e87e9ba0f2008f59a2187975ab9be56fb831b7
Shayan Dhanani <shayandhanani@gmail.com>
2014-12-02 12:32:17 -1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import re
 from lxml import etree
 from pyquery import PyQuery as pq
 
-scrape_url = "http://www.tripadvisor.com/Attractions-g737051-Activities-Cat_Ba.html"
+scrape_url = "http://www.tripadvisor.com/Attractions-g28968-Activities-Washington.html"
 
 header = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.47 Safari/536.11',
            'Cookie': 'PHPSESSID=de45029e5e2fab4f6e5eef56515d6c1c; __utma=123692957.1658163614.1349740913.1349740913.1352756518.2; __utmb=204497347.1.10.1342787814; __utmc=204497347; __utmz=204497347.1341998344.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)' }
@@ -38,5 +38,32 @@ def parse_list(root):
           email = email[0]
           print email
 
+def get_sub_loc_urls(url)
+    for el in url(".geo_name").next():
+        sub_url = "http://www.tripadvisor.com" + el.get("href")
+        print sub_url
+        
+         
+def def parse_listing_pages(start_url):
+    # not iterate over the pages
+    count = 0
+    while True:
+        url = start_url % (count) # targets each page in the list
+        print "On page %s" % url
+        root = get_url(url)
+
+        # check if there are items, if not stop since you exceeded the total pages
+        if not root(".listing"):
+            print "Reached end at page %s" % count
+            break
+
+        # this will parse the first listing page
+        parse_list(root)
+        print "Finished page %s" % count
+        count = count + 30
+
 scrape_url_page = get_url(scrape_url)
-parse_list(scrape_url_page)
+get_sub_loc_urls(scrape_url_page)
+
+
+#parse_list(scrape_url_page)
