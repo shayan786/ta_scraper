af2a329c0bebfa4c50e7eaf9d00407a91e35d2c7
Shayan Dhanani <shayandhanani@gmail.com>
2014-12-02 12:57:48 -1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -51,6 +51,8 @@ def scrape_activities_in_a_region(url):
         
         # go to the adventure page (if it exists)
         for el in sub_activities_page(".filterbox .styleButton a"):
+           print el.attr("href")
+                   
            if el.val() == "Adventure":
                sub_activities_adv_url = "http://www.tripadvisor.com" + el.attr("href")
                sub_activities_adv_page = get_url(sub_activities_adv_url)
