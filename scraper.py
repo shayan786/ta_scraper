f76153dfeaefaa8ba53caf00057e2266b952e9bd
Shayan Dhanani <shayandhanani@gmail.com>
2014-12-01 18:36:13 -1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import re
 from lxml import etree
 from pyquery import PyQuery as pq
 
-scrape_url = "http://www.tripadvisor.com/Attractions-g60616-Activities-c25-Kapaa_Kauai_Hawaii.html#TtD"
+scrape_url = "http://www.tripadvisor.com/Attractions-g293924-Activities-c38-Hanoi.html"
 
 header = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.47 Safari/536.11',
            'Cookie': 'PHPSESSID=de45029e5e2fab4f6e5eef56515d6c1c; __utma=123692957.1658163614.1349740913.1349740913.1352756518.2; __utmb=204497347.1.10.1342787814; __utmc=204497347; __utmz=204497347.1341998344.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)' }
@@ -29,7 +29,6 @@ def parse_list(root):
     """ Takes a listing page and indexes all the listings in it """
     for el in root(".listing a.property_title"):
         page_url = "http://www.tripadvisor.com" + el.get("href");
-        # print "Url: %s" % page_url
         page = get_url(page_url)
         
         email_raw = strip_tags(page(".sprite-grayEmail").next().attr("onclick"))
@@ -37,35 +36,8 @@ def parse_list(root):
         
         if email:
           email = email[0]
-          # print email
-
-def parse_listing_pages(start_url):
-    # not iterate over the pages
-    count = 0
-    while True:
-        url = start_url % (count) # targets each page in the list
-        print "On page %s" % url
-        root = get_url(url)
-
-        # check if there are items, if not stop since you exceeded the total pages
-        if not root(".listing"):
-            print "Reached end at page %s" % count
-            break
-
-        # this will parse the first listing page
-        # parse_list(root)
-        # print "Finished page %s" % count
-        count = count + 30
+          print email
           
 
 scrape_url_page = get_url(scrape_url)
-parse_listing_pages(scrape_url_page)
-    
-# email_raw = strip_tags(page(".sprite-grayEmail").next().attr("onclick"))
-
-# email = email_regex.findall(email_raw)  
-
-# if email:
-  # email = email[0]
-
-# print email
+parse_list(scrape_url_page)
