d4934c12836842b6c67b52b33042bcdd8ecae51e
Shayan Dhanani <shayandhanani@gmail.com>
2014-12-01 16:27:09 -1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import re
 from lxml import etree
 from pyquery import PyQuery as pq
 
-scrape_url = "http://www.tripadvisor.com/Attraction_Review-g60616-d1015347-Reviews-Kayak_Wailua-Kapaa_Kauai_Hawaii.html"
+scrape_url = "http://www.tripadvisor.com/Attractions-g60616-Activities-Kapaa_Kauai_Hawaii.html"
 
 header = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.47 Safari/536.11',
            'Cookie': 'PHPSESSID=de45029e5e2fab4f6e5eef56515d6c1c; __utma=123692957.1658163614.1349740913.1349740913.1352756518.2; __utmb=204497347.1.10.1342787814; __utmc=204497347; __utmz=204497347.1341998344.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)' }
@@ -25,13 +25,22 @@ def strip_tags(value):
         return re.sub(r'<[^>]*?>', '', value)
     return ""
 
+def parse_list(root):
+    """ Takes a listing page and indexes all the listings in it """
+    for el in root(".listing a.property_title"):
+        page_url = "http://www.tripadvisor.com" + el.get("href");
+        print "Url: %s" % page_url
+
+
 page = get_url(scrape_url)
+  
+parse_list(page)
     
-email_raw = strip_tags(page(".sprite-grayEmail").next().attr("onclick"))
+# email_raw = strip_tags(page(".sprite-grayEmail").next().attr("onclick"))
 
-email = email_regex.findall(email_raw)  
+# email = email_regex.findall(email_raw)  
 
-if email:
-  email = email[0]
+# if email:
+  # email = email[0]
 
-print email
+# print email
