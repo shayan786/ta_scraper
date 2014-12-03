b831fc52bb58e25706aee09fa0fb9eeccbcfad8c
Shayan Dhanani <shayandhanani@gmail.com>
2014-12-02 14:18:51 -1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import re
 from lxml import etree
 from pyquery import PyQuery as pq
 
-scrape_url = "http://www.tripadvisor.com/Attractions-g28940-Activities-Maine.html"
+scrape_url = "http://www.tripadvisor.com/Attractions-g147319-Activities-c38-Puerto_Rico.html"
 
 header = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.47 Safari/536.11',
            'Cookie': 'PHPSESSID=de45029e5e2fab4f6e5eef56515d6c1c; __utma=123692957.1658163614.1349740913.1349740913.1352756518.2; __utmb=204497347.1.10.1342787814; __utmc=204497347; __utmz=204497347.1341998344.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)' }
@@ -61,7 +61,7 @@ def scrape_activities_in_a_region(url):
         
 
 scrape_url_page = get_url(scrape_url)
-scrape_activities_in_a_region(scrape_url_page)
+#scrape_activities_in_a_region(scrape_url_page)
 
 
-#parse_list(scrape_url_page)
+parse_list(scrape_url_page)
